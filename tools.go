20e768005c1db6d22fac48320e3b42fc90a05ea7
gibran <gibran@entropy.tech>
2016-10-31 17:33:46 -0600

    Add Aggregate function.

--- tools.go
+++ tools.go
@@ -1,5 +1,6 @@
 package tools
 
+// Test if given one element is subset of an array.
 func Subset(first, second []string) bool {
 	set := make(map[string]int)
 	for _, value := range second {
@@ -19,7 +20,8 @@ func Subset(first, second []string) bool {
 	return true
 }
 
-func unique(x []string) []string {
+// Remove duplicate elements of the given array.
+func Unique(x []string) []string {
 	encountered := map[string]bool{}
 	result := []string{}
 
@@ -33,7 +35,8 @@ func unique(x []string) []string {
 	return result
 }
 
-func removeElement(element string, array []string) []string {
+// Remove array elements given element that I want remove.
+func RemoveElement(element string, array []string) []string {
 	var out []string
 	l1 := len(array)
 	for i := 0; i < l1; i++ {
@@ -46,25 +49,40 @@ func removeElement(element string, array []string) []string {
 
 // Difference returns difference between two slices of strings
 func Difference(slice1 []string, slice2 []string) []string {
-    var difference []string
+	var difference []string
 
-    for i := 0; i < 2; i++ {
-        for _, s1 := range slice1 {
-            found := false
-            for _, s2 := range slice2 {
-                if s1 == s2 {
-                    found = true
-                    break
-                }
-            }
-            if !found {
-                difference = append(difference, s1)
-            }
-        }
-        if i == 0 {
-            slice1, slice2 = slice2, slice1
-        }
-    }
+	for i := 0; i < 2; i++ {
+		for _, s1 := range slice1 {
+			found := false
+			for _, s2 := range slice2 {
+				if s1 == s2 {
+					found = true
+					break
+				}
+			}
+			if !found {
+				difference = append(difference, s1)
+			}
+		}
+		if i == 0 {
+			slice1, slice2 = slice2, slice1
+		}
+	}
 
-    return difference
+	return difference
+}
+
+func Aggregate(colgroupnames []string, colvaluename string, datasets [][]string) []int {
+	var index []int
+	//l1 := len(colgroupnames)
+	l2 := len(datasets[0])
+	//l3 := len(datasets)
+	for _, col := range colgroupnames {
+		for i := 0; i < l2; i++ {
+			if col == datasets[0][i] {
+				index = append(index, i)
+			}
+		}
+	}
+	return index
 }
--- tools_test.go
+++ tools_test.go
@@ -25,7 +25,7 @@ func TestTools(t *testing.T) {
 		// first := data[0]
 
 		Convey("If I remove [iphone] of [iphone iphone-case]", func() {
-			re := removeElement(data[0][0], data[5])
+			re := RemoveElement(data[0][0], data[5])
 			Convey("The result array is [iphone-case]", func() {
 				So(re[0], ShouldEqual, "iphone-case")
 			})
@@ -33,7 +33,7 @@ func TestTools(t *testing.T) {
 
 		Convey("When I remove duplicated element of [a  b a c]", func() {
 			x := []string{"a", "b", "a", "c"}
-			y := unique(x)
+			y := Unique(x)
 			Convey("The third element of the array is c", func() {
 				So(y[2], ShouldEqual, "c")
 			})
@@ -41,12 +41,33 @@ func TestTools(t *testing.T) {
 
 	})
 
-  Convey("Given two different slices", t, func() {
-    slice1 := []string{"1", "2", "3"}
-    slice2 := []string{"1", "2", "3", "4"}
-    Convey("It should return the difference between the two", func() {
-      result := []string{"4"}
-      So(Difference(slice1, slice2), ShouldResemble, result)
-    })
-  })
+	Convey("Given two different slices", t, func() {
+		slice1 := []string{"1", "2", "3"}
+		slice2 := []string{"1", "2", "3", "4"}
+		Convey("It should return the difference between the two", func() {
+			result := []string{"4"}
+			So(Difference(slice1, slice2), ShouldResemble, result)
+		})
+	})
+
+	Convey("xxxxxxxxxxxxxxxxxxxxxxxxxx", t, func() {
+		data := [][]string{
+			[]string{"id", "type", "value"},
+			[]string{"111", "A", "2"},
+			[]string{"111", "B", "1"},
+			[]string{"111", "A", "1"},
+			[]string{"222", "C", "2"},
+			[]string{"222", "B", "1"},
+			[]string{"111", "C", "3"},
+		}
+
+		x := []string{"id", "type"}
+		y := "value"
+
+		Convey("yyyyyyyyyyyyyyyyyyyyyyyyyyyy", func() {
+			result := Aggregate(x, y, data)
+			So(result[1], ShouldEqual, 1)
+		})
+	})
+
 }
