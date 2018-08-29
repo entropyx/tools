5e52d2ce14dd88cff54ada2f43c7f791640ad436
Daniel Montoya <dsmontoyam@gmail.com>
2018-08-29 17:10:36 -0500

    Parse any number to float so it can be easily handled.

--- maputils/maputils.go
+++ maputils/maputils.go
@@ -1,8 +1,16 @@
 package maputils
 
+import (
+	"strconv"
+)
+
 func StringToInterface(stringMap map[string]string) map[string]interface{} {
 	ifaceMap := map[string]interface{}{}
 	for k, v := range stringMap {
+		if fv, err := strconv.ParseFloat(v, 64); err == nil {
+			ifaceMap[k] = fv
+			continue
+		}
 		ifaceMap[k] = v
 	}
 	return ifaceMap
