de200fb1fb847f271dce453f6b845ee5d0779215
Daniel Montoya <dsmontoyam@gmail.com>
2019-06-11 17:14:01 -0500

    Simplify stack trace

--- debugutils/debugutils.go
+++ debugutils/debugutils.go
@@ -1,7 +1,38 @@
 package debugutils
 
-import "runtime/debug"
+import (
+	"runtime/debug"
+	"strings"
+)
+
+const (
+	stackFuncRuntime    = "runtime/debug.Stack"
+	stackFuncDebugutils = "github.com/entropyx/tools/debugutils.Stack"
+)
 
 func Stack() string {
 	return string(debug.Stack())
+}
+
+func StackSimple() string {
+	s := Stack()
+	lines := strings.Split(s, "\n")
+	newLines := []string{}
+	for i := 0; i < len(lines)-1; i++ {
+		line := lines[i]
+		if i == 0 {
+			newLines = append(newLines, line)
+		}
+		if i%2 == 0 {
+			continue
+		}
+		if strings.Contains(line, stackFuncRuntime) || strings.Contains(line, stackFuncDebugutils) {
+			continue
+		}
+		newLines = append(newLines, line, lines[i+1])
+		if len(newLines) == 7 {
+			break
+		}
+	}
+	return strings.Join(newLines, "\n")
 }
