24b29a26e555c70153928f4f783e0157dfb510d8
Daniel Montoya <dsmontoyam@gmail.com>
2020-01-21 16:49:29 -0600

    strutils: remove one mo char

--- strutils/rand.go
+++ strutils/rand.go
@@ -11,7 +11,7 @@ var allChars = append(alphaNum, symbols...)
 var alphaNum = append(letters, numbers...)
 var letters = append(lowercaseLetters, uppercaseLetters...)
 var numbers = []byte{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
-var symbols = []byte{'!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', '<', '=', '>', ';', '?', '@', '[', ']', '_', '{', '}', '|', '~', ' ', 'ñ'}
+var symbols = []byte{'!', '#', '$', '%', '&', '(', ')', '*', '+', '.', '/', '<', '=', '>', ';', '?', '@', '[', ']', '_', '{', '}', '|', '~', ' ', 'ñ'}
 var lowercaseLetters = []byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
 var uppercaseLetters = []byte{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
 
